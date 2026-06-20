e633b274dc9fdfa91ce62aaa36c8ae8776e5bf8f
alexfreitasbr <alexfreitasbr@hotmail.com>
2026-06-20 20:32:43 -0300

    docker

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     {
       transport: Transport.NATS,
       options: {
-        server: envs.natsServers,
+        servers: envs.natsServers,
       },
     },
   );
